03d241b375c1c43b2ba2f4a1e7ad20a553a07108
John O'Leary <john@studiozerozero.com>
2025-01-26 02:54:24 -0500

    a blue version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-   <h1 className="text-red-500 text-4xl flex align-middle justify-center mt-20">
-What is going on?
+   <h1 className="text-blue-500 text-4xl flex align-middle justify-center mt-20">
+What is going on? This is another branch
    </h1>
    
    </>
