d3dec1089fb90579879ee659b748448bd69af543
John O'Leary <john@studiozerozero.com>
2025-01-26 03:00:41 -0500

    mono it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-   <h1 className="text-blue-500 text-4xl flex align-middle justify-center mt-20">
+   <h1 className="text-blue-500 text-4xl flex  font-mono font-bold align-middle justify-center mt-20">
 What is going on? This is another branch, it's blue.
    </h1>
    
