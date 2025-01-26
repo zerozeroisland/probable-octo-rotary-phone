8060a69e148a7fbb7faf8e317c275714f1c446cc
John O'Leary <john@studiozerozero.com>
2025-01-26 12:36:31 -0500

    stuff happened

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-   <div className="bg-slate-300 p-8 rounded-lg shadow-md w-[50%] overflow-hidden flex justify-center mt-40 mx-auto align-middle">
-   <h1 className="text-blue-500 text-4xl flex  font-mono font-bold align-middle justify-center p-8">
+   <div className="bg-slate-300 p-8 rounded-lg shadow-md border-4 border-yellow-800 w-[50%] overflow-hidden flex justify-center mt-40 mx-auto align-middle">
+   <h1 className="text-blue-500 text-4xl flex  font-family- font-bold align-middle text-center justify-center p-8">
 What is going on? This is another branch, it's blue.
    </h1>
    </div>
