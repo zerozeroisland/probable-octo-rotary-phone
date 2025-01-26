8ad54cb821afca4d14f13a46cf027fe74801b464
John O'Leary <john@studiozerozero.com>
2025-01-26 03:07:59 -0500

    made a card, a bad one.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-   <h1 className="text-blue-500 text-4xl flex  font-mono font-bold align-middle justify-center mt-20">
+   <div className="bg-slate-300 p-8 rounded-lg shadow-md w-[50%] overflow-hidden flex justify-center mt-40 mx-auto align-middle">
+   <h1 className="text-blue-500 text-4xl flex  font-mono font-bold align-middle justify-center p-8">
 What is going on? This is another branch, it's blue.
    </h1>
-   
+   </div>
    </>
   );
 }
