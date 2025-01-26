772894dc54cc01c332e09fae30914431d68487a7
John O'Leary <john@studiozerozero.com>
2025-01-26 02:56:56 -0500

    more txt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <>
    <h1 className="text-blue-500 text-4xl flex align-middle justify-center mt-20">
-What is going on? This is another branch
+What is going on? This is another branch, it's blue.
    </h1>
    
    </>
